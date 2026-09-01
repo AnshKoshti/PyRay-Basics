e9263eae73fe91a4321137fcad8902421d6446b9
Ansh Koshti <koshtiwork26@gmail.com>
2026-09-01 17:10:37 +0530

    loading and drawing texture.

--- basics.py
+++ basics.py
@@ -3,6 +3,9 @@ from raylib import *
 
 init_window(800, 600, "Base")
 
+# Importing things.
+spaceship_texture = load_texture("assets/spaceship.png")
+
 while not window_should_close():
     begin_drawing()
     clear_background(BLACK)
@@ -14,6 +17,9 @@ while not window_should_close():
     draw_circle_v(Vector2(400, 450), 30, BLUE)
     draw_line(100, 200, 300, 400, YELLOW)
     draw_line_ex(Vector2(150, 250), Vector2(350, 450), 10, ORANGE)
+
+    # Display images.
+    draw_texture(spaceship_texture, 0, 0, WHITE)
 
     end_drawing()
 close_window()

76f334daccea44130c80b74dcf25d3224c400769
Ansh Koshti <koshtiwork26@gmail.com>
2026-09-03 17:47:42 +0530

    Fixing screen bounding size.

--- movement.py
+++ movement.py
@@ -4,12 +4,14 @@ from os.path import join
 
 # Windows defind parameters.
 set_config_flags(FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE)
+
+# initializing window.
 init_window(800, 600, "Move")
 
 # Only used in certain conditions, not ideal for frame consistent games.
 # set_target_fps(60)
 
-# Variables for import and definations.
+# Variables for import and textures.
 spaceship = load_texture(join("assets", "spaceship.png"))
 ship_pos = Vector2(0, 0)
 ship_direction = Vector2(1, 1)
@@ -18,9 +20,9 @@ ship_speed = 100
 while not window_should_close():
 
     # Ship edge bounding.
-    if ship_pos.x >= 1920:
+    if ship_pos.x >= 800:
         ship_direction.x = -1
-    if ship_pos.y >= 1080:
+    if ship_pos.y >= 600:
         ship_direction.y = -1
     if ship_pos.x <= 0:
         ship_direction.x = 1

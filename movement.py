e49604dbdd37082968e493ef5987a97c7de9e6d3
Ansh Koshti <koshtiwork26@gmail.com>
2026-09-03 17:03:35 +0530

    Setting up movement system to frame consistant with delta time.

--- movement.py
+++ movement.py
@@ -6,17 +6,27 @@ from os.path import join
 set_config_flags(FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE)
 init_window(1920, 1080, "Move")
 
+# Only used in certain conditions, not ideal for frame consistent games.
+# set_target_fps(60)
+
 # Variables for import and definations.
 spaceship = load_texture(join("assets", "spaceship.png"))
-pos_x = 0
-pos_y = 0
+ship_pos = Vector2(0, 0)
+ship_direction = Vector2(1, 1)
+ship_speed = 100
 
 while not window_should_close():
+
+    # Movement update.
+    dt = get_frame_time()
+    ship_pos.x += ship_direction.x * ship_speed * dt
+    ship_pos.y += ship_direction.y * ship_speed * dt
 
     # Drawing functions.
     begin_drawing()
     clear_background(BLACK)
-    draw_texture(spaceship, pos_x, pos_y, WHITE)
+    draw_texture_v(spaceship, ship_pos, WHITE)
+    draw_fps(0, 0)
     end_drawing()
 
 close_window()

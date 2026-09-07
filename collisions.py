6906f09d9dabdeb22f70a07f32889d3b82bad051
Ansh Koshti <koshtiwork26@gmail.com>
2026-09-07 08:20:25 +0530

    Setting up base for collision.

---
+++ collisions.py
@@ -0,0 +1,24 @@
+from pyray import *
+from raylib import *
+
+set_config_flags(FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE)
+init_window(1920, 1080, "Raylib collisions.")
+
+player_pos = Vector2(0, 0)
+obstacle_pos = Vector2(500, 400)
+player_radius = 50
+obstacle_radius = 30
+
+while not window_should_close():
+
+    # Input
+    player_pos = get_mouse_position()
+
+    # Drawing.
+    begin_drawing()
+    clear_background(BLACK)
+    draw_circle_v(player_pos, player_radius, WHITE)
+    draw_circle_v(obstacle_pos, obstacle_radius, RED)
+    end_drawing()
+
+close_window()

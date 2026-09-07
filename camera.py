c420b84e35ed9ba4950be5f7cb2b23113cd22535
Ansh Koshti <koshtiwork26@gmail.com>
2026-09-07 17:45:02 +0530

    Basic camera setup.

---
+++ camera.py
@@ -0,0 +1,45 @@
+from pyray import *
+from raylib import *
+from random import randint, choice
+
+set_config_flags(FLAG_WINDOW_RESIZABLE)
+init_window(800, 600, "Raylib camera.")
+
+# Player variables.
+pos = Vector2()
+radius = 50
+direction = Vector2()
+speed = 200
+
+circles = [
+    (
+        Vector2(randint(-2000, 2000), randint(-1000, 1000)),
+        randint(50, 200),
+        choice([RED, GREEN, BLUE, YELLOW, ORANGE]),
+    )
+    for i in range(100)
+]
+
+camera = Camera2D()
+
+while not window_should_close():
+
+    # Input.
+    direction.x = int(is_key_down(KEY_RIGHT)) - int(is_key_down(KEY_LEFT))
+    direction.y = int(is_key_down(KEY_DOWN)) - int(is_key_down(KEY_UP))
+    direction = vector2_normalize(direction)
+
+    # Movement.
+    dt = get_frame_time()
+    pos.x += direction.x * speed * dt
+    pos.y += direction.y * speed * dt
+
+    # Drawing.
+    begin_drawing()
+    clear_background(WHITE)
+    for circle in circles:
+        draw_circle_v(*circle)
+    draw_circle_v(pos, radius, BLACK)
+    end_drawing()
+
+close_window()
